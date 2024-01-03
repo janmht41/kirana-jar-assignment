f6b44af40b13042746de13d34a8718befc7a88f9
Mohit Jain <mohitjain@Mohits-MacBook-Air.local>
2024-01-03 21:29:59 +0530

    added javadoc for Entity class

--- src/main/java/com/assignment/KiranaService/entity/Transaction.java
+++ src/main/java/com/assignment/KiranaService/entity/Transaction.java
@@ -13,6 +13,10 @@ import lombok.NoArgsConstructor;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+/**
+ * Represents a transaction entity in the Kirana Service.
+ * This entity is mapped to the "KIRANA_TRANSACTIONS" table in the database.
+ */
 @Data
 @Builder
 @Entity
@@ -20,25 +24,47 @@ import java.util.UUID;
 @AllArgsConstructor
 @Table(name = "KIRANA_TRANSACTIONS")
 public class Transaction {
+
+    /**
+     * Unique identifier for the transaction.
+     */
     @Id
     @Column(name="TRAN_ID")
     private UUID transactionId;
 
+    /**
+     * Amount debited in INR. Default value is 0.
+     */
     @Column(name="DEBIT_AMT_INR", columnDefinition="double default 0")
     private Double debitAmount;
 
+    /**
+     * Date and time of the transaction.
+     */
     @Column(name="TRAN_DATETIME")
     private LocalDateTime transactionTime;
 
+    /**
+     * Amount credited in INR. Default value is 0.
+     */
     @Column(name="CREDIT_AMT_INR", columnDefinition="double default 0")
     private Double creditAmount;
 
+    /**
+     * Details about the transaction.
+     */
     @Column(name="TRAN_DESC")
     private String transactionDetails;
 
+    /**
+     * Payment mode used for the transaction.
+     */
     @Column(name = "PAYMENT_MODE")
     private String paymentMode;
 
+    /**
+     * User who created the transaction.
+     */
     @Column(name = "CREATED_BY")
     private String creationUser;
 }
