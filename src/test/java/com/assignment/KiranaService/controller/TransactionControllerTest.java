5a08f1ba3f486c82339ed4e8789b9befc5565e9f
Mohit Jain <mohitjain@Mohits-MacBook-Air.local>
2024-01-04 11:40:15 +0530

    added test for invalid transaction currency

--- src/test/java/com/assignment/KiranaService/controller/TransactionControllerTest.java
+++ src/test/java/com/assignment/KiranaService/controller/TransactionControllerTest.java
@@ -1,12 +1,15 @@
 package com.assignment.KiranaService.controller;
 
+import com.assignment.KiranaService.model.TransactionRequestModel;
 import com.assignment.KiranaService.service.TransactionServiceImpl;
 import com.assignment.KiranaService.model.TransactionSummaryDto;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.sql.Date;
@@ -16,6 +19,7 @@ import java.util.Collections;
 
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -24,9 +28,11 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class TransactionControllerTest {
     @Autowired
     private MockMvc mockMvc;
-
+    @Autowired
+    private ObjectMapper objectMapper;
     @MockBean
     private TransactionServiceImpl transactionService;
+
     @Test
     void getTransactions_shouldReturnListOfTransactions() throws Exception {
         LocalDate currentDate = LocalDate.now();
@@ -41,6 +47,8 @@ public class TransactionControllerTest {
         verify(transactionService, times(1)).getTransactionsByDate(currentDate);
     }
 
+
+
     @Test
     void getSummary_shouldReturnTransactionSummary() throws Exception {
         LocalDate currentDate = LocalDate.now();
@@ -53,6 +61,25 @@ public class TransactionControllerTest {
                 .andExpect(jsonPath("$").isMap());
 
         verify(transactionService, times(1)).getTransactionSummaryOn(currentDate);
+    }
+
+    @Test
+    void shouldThrowErrorForInvalidCurrency() throws Exception {
+        TransactionRequestModel transactionRequestModel = TransactionRequestModel.builder()
+                .transactionDesc("dummy")
+                .transactionType("CREDIT")
+                .amount(38.32)
+                .currency("CAD")
+                .paymentMode("CASH")
+                .creationUser("dummy")
+                .build();
+
+        doThrow(IllegalArgumentException.class).when(transactionService).saveTransaction(any());
+
+        mockMvc.perform(post("/record")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(transactionRequestModel)))
+                .andExpect(status().is4xxClientError());
     }
     private TransactionSummaryDto createMockTransactionSummary() {
         return TransactionSummaryDto.builder()
